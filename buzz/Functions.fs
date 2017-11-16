1ac5a8cf5d7b6c15348f4f9a37bf86b6002334a5
Luca Di Stefano <luca.distefano@gssi.it>
2017-11-16 12:48:37 +0100

    link() function

--- buzz/Functions.fs
+++ buzz/Functions.fs
@@ -6,3 +6,11 @@
         let timeof ( tv: Tval ) = snd tv
 
         let initLoc p = Map.empty<string, AttrVal>.Add("loc", p)
+
+        let link(l1: AttrVal, l2: AttrVal) =
+            let DELTA = 1.0
+
+            match (l1, l2) with
+            | (Int(a), Int(b)) -> (float >> abs) (a - b) <= DELTA
+            | (P(p1), P(p2)) -> d(p1, p2) <= DELTA
+            | _ -> false
