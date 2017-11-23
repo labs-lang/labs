092371d44ff60428e71e913fb6c5cec85581a5fb
Luca Di Stefano <luca.distefano@gssi.it>
2017-11-23 16:08:35 +0100

    Make link() a curried function, add torusLoc()

--- buzz/Functions.fs
+++ buzz/Functions.fs
@@ -7,10 +7,20 @@
 
         let initLoc p = Map.empty<string, Val>.Add("loc", p)
 
-        let link(l1: Val, l2: Val) =
+        let link l1 l2 =
             let DELTA = 1.0
 
             match (l1, l2) with
             | (Int(a), Int(b)) -> (float >> abs) (a - b) <= DELTA
             | (P(p1), P(p2)) -> d(p1, p2) <= DELTA
             | _ -> false
+
+        let rec torusLoc p:Point =
+            let Xmax = 10
+            let Ymax = 10
+            match p with
+            | (x, y) when x<0 -> torusLoc (Xmax, y)
+            | (x, y) when y<0 -> torusLoc (x, Ymax)
+            | (x, y) when x>Xmax -> torusLoc (0, y)
+            | (x, y) when y>Ymax -> torusLoc (x, 0)
+            | _ -> p
