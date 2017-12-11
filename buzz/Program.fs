fe3afaba2cad3da0e5385a520df5f1e19bce392d
Luca Di Stefano <luca.distefano@gssi.it>
2017-12-11 14:42:04 +0100

    Add functions for "bouncing" behavior

--- buzz/Program.fs
+++ buzz/Program.fs
@@ -12,6 +12,34 @@ open Chiron
 let print x =
     printfn "----\n%A" (x)
     x
+   
+
+let triangle lim n = 
+    let twiceLim = lim * 2
+    let x = modulo n twiceLim
+    if x <= lim then x else twiceLim-x 
+let trPoint triangleX triangleY (x,y) =
+    (triangleX x, triangleY y)
+
+let changeDir changePoint (dx,dy) (x,y) =
+    let (x', y') = changePoint (x,y)
+    ((if x-x' <> 0 then -dx else dx), if y-y' <> 0 then -dy else dy)
+
+let bounce xMax yMax (c:Comp) =
+    let updatePoint = trPoint (triangle xMax) (triangle yMax)
+
+    let loc = c.I.["loc"]
+    let dir = c.I.["dir"]
+    match (loc, dir) with
+    | P(l), P(d) -> 
+    {c with I = c.I.Add("loc", P(updatePoint l)).Add("dir", P(changeDir updatePoint d l))}
+    | _ -> c
+
+let bounceL xMax yMax c =
+    let c' = bounce xMax yMax c
+    if c'.I.["dir"] <> c.I.["dir"]
+    then {c' with P = LazyPut("dir", I("dir"))^.c'.P}
+    else c'
 
 /// n components move in random directions. When two component "meet", they
 /// start moving in the same direction.
@@ -19,7 +47,8 @@ let print x =
 let swarm argv = 
     let proc = LazyPut("dir", I("dir")) ^. RecX(Attr("loc", Sum(I("loc"),L("dir"))) ^. X)
     let n = 10
-    let xMax, yMax = 10, 10
+    let xMax, yMax = 50, 50
+
 
     let directions = seq {for x in [1..n] do yield randomDirection()}
     let points = seq {for x in [1..n] do yield P(rng.Next(xMax+1), rng.Next(yMax+1))}
