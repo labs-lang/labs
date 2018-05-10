57ca116d724e3ea94fd1cd8002434f835163c94e
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-10 17:39:07 +0200

    Add some basic parsers

--- LabsParser/Common.fs
+++ LabsParser/Common.fs
@@ -8,13 +8,17 @@ type Parser<'t> = Parser<'t, unit>
 //-----------------------------------------
 let INTERFACE : Parser<_> =     (pchar 'I')
 let LSTIG : Parser<_> =         (pchar 'L')
+let ENV : Parser<_> =           (pchar 'E')
 let NEG : Parser<_> =           (pchar '!')
 let CONJ : Parser<_> =          (pchar '&')
 let ASSIGN : Parser<_> =        (pstring ":=")
 let AWAIT : Parser<_> =         (pchar '?')
-let PREFIX : Parser<_> =        (pchar '.')
+let SEQ : Parser<_> =           (pchar ';')
+let PAR : Parser<_> =           (pchar '|')
+let CHOICE : Parser<_> =        (pchar '+')
 //-----------------------------------------
 
+let IDENTIFIER : Parser<_> = (regex "[A-Z][A-Za-z0-9]*")
 let ws = spaces
 let btw beginchar endchar : Parser<_> = 
     between (pchar beginchar) (pchar endchar) (manySatisfy ((<>) endchar))
@@ -26,6 +30,7 @@ let andspaces p = p >>. spaces
 
 let interfaceKey = INTERFACE >>. betweenBrackets
 let lstigKey = LSTIG >>. betweenBrackets
+let envKey = ENV >>. betweenBrackets
 
 /// Apply parser p1, then apply optional parser p2.
 /// If p2 succeeds, pass both results to if2.
