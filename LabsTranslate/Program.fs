648d7653db8fd8d2dd41cfbd4e2ba83484e00c72
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-10 17:42:52 +0200

    LabsTranslate.main first commit

---
+++ LabsTranslate/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+open FParsec
+open StepResult
+open Checks
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    // return an integer exit code
+
+    File.ReadAllText("../examples/test.labs")
+    |> log parse
+    >>= log uniqueDefs
+    >>= log checkNames
+    >>= log checkInit
+    |> ignore
+
+
+    0
