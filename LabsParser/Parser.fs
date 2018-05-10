e0f28739ea33a8f9837acd625180c42fba20e834
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-10 17:40:14 +0200

    Parser facade; add comments parser

--- LabsParser/Parser.fs
+++ LabsParser/Parser.fs
@@ -1,5 +1,22 @@
 ﻿module Parser
 
 open FParsec
+open Labs.Types
 
-let pproc = Processes.pproc
+let pProc = Processes.pproc
+
+type private Line =
+    | Def of string * Process
+    | Comment of unit
+
+let lineComment : Parser<_> = (skipChar '#') >>. skipRestOfLine false .>> ws
+
+let pdef = 
+
+    (IDENTIFIER .>> ws ) .>>. (pchar '=' >>. ws >>. pProc) .>> ws
+
+let full = 
+    (ws >>. many ((pdef |>> Def) <|> (lineComment |>> Comment)) .>> (ws .>> eof))
+    |>> List.choose (function Def(a,b) -> Some (a,b) | _ -> None)
+
+let pInit = Processes.pInit
