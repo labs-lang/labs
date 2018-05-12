a2d7706201f6a2c5cdef56e3dba609d6e84379a0
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-12 08:51:30 +0200

    Translate.Check: Use System.Result

--- LabsTranslate/Checks.fs
+++ LabsTranslate/Checks.fs
@@ -1,7 +1,8 @@
 ﻿module Checks
-open Labs.Types
-open StepResult
-open FParsec
+open Types
+open Base
+
+let withcommas : seq<string> -> string = (String.concat ",")
 
 /// Verifies that no process is defined more than once.
 let uniqueDefs processes = 
@@ -12,13 +13,13 @@ let uniqueDefs processes =
         |> List.filter (fun (x, y) -> y.Length > 1)
         |> List.map fst
     if duplicates.Length = 0 then
-        Continue(processes, "All definitions are unique")
+        Result.Ok processes
     else
-        Fail(sprintf "The following processes have multiple definitions: %A" duplicates)
+        Result.Error (sprintf "The following processes have multiple definitions: %A" duplicates)
 
 /// Verifies that all process names in the program have been defined.
 let checkNames processes =
-    let definedNames = processes |> List.map fst |> Set.ofList
+    let definedNames = fstSet processes
 
     let rec usedNames = function
     | Name(n) -> Set.singleton n
@@ -35,9 +36,10 @@ let checkNames processes =
         |> Set.unionMany
         |> (fun x -> Set.difference x definedNames)
 
-    if undefinedNames.Count = 0 then Continue(processes, "All names are defined")  else 
-    Fail(sprintf "The following processes are undefined: %A" undefinedNames)
+    if undefinedNames.Count = 0 then Result.Ok processes
+    else Result.Error (sprintf "The following processes are undefined: %s" (withcommas undefinedNames))
 
+///Checks that the Init oricess (if any) is a sequence of environment writes
 let checkInit (processes: (string * Process) list) = 
 
     let rec checkExpr = function
@@ -63,11 +65,25 @@ let checkInit (processes: (string * Process) list) =
 
     let resultOf p =
         let messages = check p
-        if messages.Count = 0 then Continue(processes, "Init ok")
-        else Fail("Invalid constructs in Init proces: " + String.concat ", " messages)
+        if messages.Count = 0 then Result.Ok processes
+        else Result.Error ("Invalid constructs in Init proces: " + withcommas messages)
 
     processes
     |> List.tryFind(fun x -> (fst x) = "Init")
     |> Option.map snd
     |> Option.map resultOf
-    |> Option.defaultValue (StepResult.Continue(processes, "Init ok (No Init process)"))
+    |> Option.defaultValue (Result.Ok processes)
+
+let checkComponents (processes, (components: (int32 * string) list)) =
+    if components.IsEmpty then Result.Error ("No components defined")
+    else
+        let names = fstSet processes
+        let allDefined = 
+            components
+            |> List.map snd
+            |> List.forall names.Contains
+        if allDefined then
+            Result.Ok (processes, components)
+        else
+            // Todo 
+            Result.Error("Some components are invalid")
