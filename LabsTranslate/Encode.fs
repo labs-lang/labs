f1aeca8d199d87678ac4a742caa17c4158953f3e
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-12 08:52:02 +0200

    Encode, Templates: First commit

---
+++ LabsTranslate/Encode.fs
@@ -0,0 +1,85 @@
+﻿module Encode
+open Types
+open Base
+
+type Node = 
+    //| Inner of int * Process * int
+    | Check of int * BExpr * int
+    | Leaf of int * Action * int
+    | Goto of int * int
+    | Stop of int
+
+let makeCounter (init: int) =
+    let x = ref init
+    let incr() =
+        x := !x + 1
+        !x
+    incr
+
+/// Assigns a unique int to each key in the program.
+let enumerateKeys processes =
+    let rec allKeysExpr = function
+    | I(x) -> Set.singleton x
+    | L(x) -> Set.singleton x
+    | Sum(e1, e2) -> Set.union (allKeysExpr e1) (allKeysExpr e2)
+    | _ -> Set.empty
+
+    let rec allKeysBexpr = function
+    | Compare(e1, _, e2) -> Set.union (allKeysExpr e1) (allKeysExpr e2)
+    | NilCheck(e) -> allKeysExpr e
+    | Neg(b) -> allKeysBexpr b
+    | Conj(b1, b2) -> Set.union (allKeysBexpr b1) (allKeysBexpr b2)
+    | _ -> Set.empty
+
+    let rec allKeys = function
+    | Base(AttrUpdate(a, e))
+    | Base(LStigUpdate(a, e))
+    | Base(EnvWrite(a, e)) -> (allKeysExpr e).Add a
+    | Base(EnvRead(a, b)) -> (Set.singleton a).Add b
+    | Par(p, q) 
+    | Choice(p, q)
+    | Seq(p, q) -> Set.union (allKeys p) (allKeys q)
+    | Await(b, p) -> Set.union (allKeysBexpr b) (allKeys p)
+    | Name(_)
+    | Nil
+    | Skip -> Set.empty
+
+    processes
+    |> List.map allKeys
+    |> Set.unionMany
+    |> Seq.mapi (fun i x -> x,i)
+    |> Map.ofSeq
+
+
+
+
+//let makeTree (processes: Map<string, Process>) p =
+    //let pcProcesses = 
+    //    processes
+    //    |> Map.toSeq
+    //    |> fstSet
+    //    |> Seq.mapi (fun i x -> x,i)
+    //    |> Map.ofSeq
+
+    //let maxPc = Map.fold (fun x _ pc -> max x pc) 0 pcProcesses
+    //let count = makeCounter(maxPc)
+
+
+    //let rec visit i j = function
+    //    | Base(a) -> Set.singleton <| Leaf(i, a, j)
+    //    | Seq(p,q) -> 
+    //         let k = count()
+    //         Set.union (visit i k p) (visit k j q)
+    //    | Await(b, p) ->
+    //         let k = count()
+    //         (visit k j p).Add(Check(i, b, k))
+    //    | Choice(p, q) ->
+    //        Set.union (visit i j p) (visit i j q)
+    //    | Name(s) ->
+    //        Set.singleton <| Skip(i, (pcProcesses.Item s))
+    //    | Tick -> Set.singleton <| Skip(i, j)
+    //    | Nil -> Set.singleton <| Stop i
+    //    | Par(p,q) ->
+
+    //visit (count()) (count()) p
+ 
---
+++ LabsTranslate/Templates.fs
@@ -0,0 +1,48 @@
+﻿module Templates
+open Types
+
+//let rec translateExpr enum = function
+//    | Const(Int(i)) -> sprintf "%i" i
+//    | Const(String(s)) -> "\"" + s + "\""
+//    | Const(P(p1,p2)) -> ""
+//    | I(k) -> sprintf "comp[tid].I[%i]" (enum.Item k)
+//    | L(k) -> sprintf "comp[tid].Lvalue[%i]" (enum.Item k)
+//    | Sum(e1, e2) -> sprintf "( (%s) + (%s) )" (translateExpr e1) (translateExpr e2)
+
+//let rec lstigKeys = function
+//    | L(x) -> Set.singleton x
+//    | Sum(e1, e2) -> Set.union (lstigKeys e1) (lstigKeys e2)
+//    | _ -> Set.empty
+
+//let updateKq enum expr = 
+    //expr
+    //|> lstigKeys
+    //|> Seq.map (fun x -> sprintf "setHin(tid, %i);" (enum.Item x))
+    //|> String.concat "\n"
+
+//let attr pc k expr enum next = 
+//    sprintf """
+//void stmt_attr%i (int tid) {
+//    __VERIFIER_assume(comp[tid].pc == %i);
+//    int val = %s;
+//    attr(tid, %i, val, -1);
+//    %s
+
+//    comp[tid].pc = %i
+//}""" pc pc (translateExpr enum expr) k (updateKq enum expr) next
+
+/// Encodes the skip process.
+let skip pc next = 
+    sprintf """
+void stmt_nop%i (int tid) {
+    __VERIFIER_assume(comp[tid].pc == %i);
+    comp[tid].pc == %i;
+}""" pc pc next
+
+/// Encodes the stop process
+let stop pc = 
+    sprintf """
+void stmt_nop%i (int tid) {
+    __VERIFIER_assume(comp[tid].pc == %i);
+    __VERIFIER_assume(0);
+}""" pc pc
