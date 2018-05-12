2127c82a1dd6917a1cb042e3b8c465c957487ef8
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-12 08:53:39 +0200

    Update Program.fs

--- LabsTranslate/Program.fs
+++ LabsTranslate/Program.fs
@@ -1,20 +1,53 @@
 ﻿// Learn more about F# at http://fsharp.org
-
-open System.IO
-open FParsec
-open StepResult
+open Base
 open Checks
+open Encode
+
+type args = {
+    help: bool; 
+    atomicLstig: bool; 
+    filePath: string;
+    verbose: bool
+}
+let defaults = {help = false; atomicLstig = false; filePath = ""; verbose = false}
+
+let rec parseArgs partial args =
+    match args with
+    | [] -> Result.Ok(partial)
+    | "--help" :: tail
+    | "-h" :: tail -> Result.Ok({partial with help = true})
+    | fpath :: tail when partial.filePath = "" -> 
+        parseArgs {partial with filePath = fpath} tail
+    | fpath :: tail -> Result.Error("Multiple files specified")
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     // return an integer exit code
 
-    File.ReadAllText("../examples/test.labs")
-    |> log parse
-    >>= log uniqueDefs
-    >>= log checkNames
-    >>= log checkInit
+    //let args = argv |> List.ofSeq |> parseArgs defaults
+    let args = Result.Ok {
+        help=false; 
+        atomicLstig=false;
+        filePath="../examples/test.labs";
+        verbose=false
+    }
+
+    args 
+    |> Result.map (fun x -> x.filePath)
+    >>= readFile
+    >>= parse
+    |> (fun x -> printf "%A" x; x)
+    |> log "Parse successful"
+    .>>= uniqueDefs
+    |> log "All definitions are unique"
+    .>>= checkNames
+    |> log "All names are defined"
+    .>>= checkInit
+    |> log "Init valid"
+    >>= checkComponents
+    |> Result.map (fun (procs, comps) -> (procs, comps, enumerateKeys (List.map snd procs)) )
+    ///|> logErr // Log any error at the end
     |> ignore
 
 
