2f473a1f09feb2fe568adf7736ee0463ce1eb257
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-17 23:25:18 +0200

    Helpers for lists, maps

--- LabsCore/Functions.fs
+++
@@ -1,8 +0,0 @@
-﻿module Functions
-
-/// Returns a list of elements that appear more than once in lst.
-let duplicates lst = 
-        lst
-        |> List.groupBy id
-        |> List.filter (fun (x, y) -> y.Length > 1)
-        |> List.map fst
---
+++ LabsCore/Map.fs
@@ -0,0 +1,10 @@
+﻿module Map
+/// Returns the set of keys in m.
+let keys m =
+    m |> Map.toSeq |> Seq.map fst |> Set.ofSeq
+/// Returns a sequence of all the values in m.
+let values m =
+    m |> Map.toSeq |> Seq.map snd
+
+let merge m2 m =
+    Map.fold (fun acc key value -> Map.add key value acc) m m2
