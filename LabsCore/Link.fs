8f2bb079355494835358b28fd5002a8351ff08a4
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-19 23:33:32 +0200

    Core: Add types for Link

---
+++ LabsCore/Link.fs
@@ -0,0 +1,20 @@
+﻿module Link
+open Types
+
+
+type LinkTerm =
+| ConstTerm of Val
+| KeyRefC1 of k:string
+| KeyRefC2 of k:string
+
+type LinkExpr =
+| T of LinkTerm
+| Abs of LinkExpr
+| Arithm of LinkExpr * ArithmOp * LinkExpr
+
+type Link = 
+| True
+| Compare of LinkExpr * CmpOp * LinkExpr
+| Neg of Link
+| Conj of Link * Link
+
