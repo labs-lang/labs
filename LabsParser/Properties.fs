f9ebf5a99ae979a820c5b3795952e0b0051c4a69
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-19 23:49:14 +0200

    Add parser for properties

---
+++ LabsParser/Properties.fs
@@ -0,0 +1,46 @@
+﻿module internal Properties
+open Types
+open Expressions
+open FParsec
+
+//let ppropTerm, ppropTermRef = createParserForwardedToRef()
+let pprop, ppropRef = createParserForwardedToRef()
+
+let ppropTerm =
+    let pkeyref = 
+        (ws KEYNAME) .>> ws (skipString "of") .>>. (ws KEYNAME) |>> KeyRef
+    choice [
+        pkeyref;
+        pval |>> ConstTerm
+    ]
+
+let pbaseprop = 
+    ppropTerm .>>. (ws EQ >>. ppropTerm) |>> Prop
+
+let pQuantifier str pType = 
+    tuple3
+        (ws (skipString str) >>. (ws IDENTIFIER))
+        ((ws KEYNAME) .>> ws (skipChar ','))
+        pprop
+    |>> pType
+do ppropRef :=
+    let pAll = pQuantifier "All" All
+    let pSome = pQuantifier "Some" Exists
+    ws (choice [pAll; pSome; pbaseprop])
+
+
+let ptemp : Parser<_> = 
+    let ptemptype = 
+        choice [
+            ws (stringReturn "Finally" Finally);
+            ws (stringReturn "Always" Always)]
+    ptemptype .>>. (ws pprop) |>> (fun (x,y) -> x y)
+
+let pproperties = 
+    (ws IDENTIFIER .>>. (ws EQ >>. ptemp))
+    |> many
+    |> pMap
+    |> (>>.) (spaces >>. manyComments)
+    |> ws
+    |> betweenBraces
+    |> (>>.) (ws (skipString "check"))
