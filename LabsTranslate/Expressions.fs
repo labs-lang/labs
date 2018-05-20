74cb7953e83953029751497376aab92adf4decfb
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-20 15:21:31 +0200

    Expressions: type inference, arithmetic ops

--- LabsTranslate/Expressions.fs
+++ LabsTranslate/Expressions.fs
@@ -1,46 +1,66 @@
 ﻿module Expressions
 open Types
 open Base
+open Templates
 
-let rec translateExpr (mapping:Map<(string * TypeofKey), int>) = function
-| Const(Int(i)) -> sprintf "%i" i
-//| Const(String(s)) -> "\"" + s + "\""
-| Const(Val.P(p1,p2)) -> ""
-| K(k) when mapping.ContainsKey (k, I)  -> 
-    sprintf "comp[tid].I[%i]" mapping.[k,I]
-| K(k) when mapping.ContainsKey (k, L)  -> 
-    sprintf "comp[tid].Lvalue[%i]" mapping.[k,L]
-| K(k) when mapping.ContainsKey (k, E)  -> 
-    // TODO
-    sprintf "E[%i]" mapping.[k,E]
-| K(k) -> failwith "Unexpected key"
-| Sum(e1, e2) -> 
-    sprintf "( (%s) + (%s) )" 
-        (translateExpr mapping e1) (translateExpr mapping e2)
+let translateAOp = function
+| Plus -> "+"
+| Minus -> "-"
+| Times -> "*"
+| Mod -> "%"
+
+
+let rec inferType (types:Map<Key,Val>) expr =
+    match expr with
+    | Const(Int(_)) -> Int(0)
+    | Const(P(_)) -> P(0,0)
+    | K(k) -> types.[k]
+    | Arithm(e1, op, e2) -> 
+        match (inferType types e1),(inferType types e2) with
+        | P(_), P(_) -> P(0,0)
+        | Int(_), Int(_) -> Int(0)
+        | _ -> failwith (sprintf "Incorrect operation: %s" (expr.ToString()))
+
+let rec translateExpr types (mapping:KeyMapping) expr =
+    let trexp = translateExpr types mapping
+    match expr with
+    | Const(Int(i)) -> sprintf "%i" i
+    //| Const(String(s)) -> "\"" + s + "\""
+    | Const(Val.P(p1,p2)) -> ""
+    | K(k) -> translateKey mapping "tid" k
+    | Arithm(e1, op, e2) -> 
+        match (inferType types expr) with
+        | Int(_) -> sprintf "( (%s) %s (%s) )" (trexp e1) (translateAOp op) (trexp e2)
+        | P(_) -> sprintf "sum(%s, %s)" (trexp e1) (trexp e2) // TODO
 
 let translateBOp = function
 | Less -> "<"
 | Equal -> "=="
 | Greater -> ">"
+| Leq -> "<="
 
-let rec translateBExpr (mapping:Map<(string * TypeofKey), int>) = function
-| True -> "true"
-| False -> "false"
-| Neg(b) -> sprintf "!(%s)" (translateBExpr mapping b)
-| Conj(b1, b2) -> 
-    sprintf "((%s) && (%s))" 
-        (translateBExpr mapping b1) (translateBExpr mapping b2)
-| Compare(e1, op, e2) ->
-    sprintf "((%s) %s (%s))"
-        (translateExpr mapping e1)
-        (translateBOp op)
-        (translateExpr mapping e2)
+let rec translateBExpr types (mapping:Map<(string * TypeofKey), int>) =
+    let trbexp = translateBExpr types mapping
+    function
+    | True -> "true"
+    | False -> "false"
+    | Neg(b) -> sprintf "!(%s)" (trbexp b)
+    | Conj(b1, b2) -> 
+        sprintf "((%s) && (%s))" 
+            (trbexp b1) (trbexp b2)
+    | Compare(e1, op, e2) ->
+        sprintf "((%s) %s (%s))"
+            (translateExpr types mapping e1)
+            (translateBOp op)
+            (translateExpr types mapping e2)
 
 
 let rec getLstigKeys (mapping:Map<(string * TypeofKey), int>) = function
 | K(k) when mapping.ContainsKey (k,L) -> (k,L) |> Set.singleton
-| Sum(e1, e2) -> Set.union (getLstigKeys mapping e1) (getLstigKeys mapping e2)
+| Arithm(e1, _, e2) -> Set.union (getLstigKeys mapping e1) (getLstigKeys mapping e2)
 | _ -> Set.empty
 
 let lstigKeys (mapping:Map<(string * TypeofKey), int>) expr =
     expr |> getLstigKeys mapping |> Set.map (fun x -> mapping.[x])
+
+
