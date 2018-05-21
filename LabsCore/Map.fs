ca6faf9ce5439054615f84efdfe806601df87872
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-21 14:55:55 +0200

    Add mapi, mapiFrom extensions to Map

--- LabsCore/Map.fs
+++ LabsCore/Map.fs
@@ -1,10 +1,28 @@
 ﻿module Map
 /// Returns the set of keys in m.
-let keys m =
-    m |> Map.toSeq |> Seq.map fst |> Set.ofSeq
+let keys table =
+    table |> Map.toSeq |> Seq.map fst |> Set.ofSeq
 /// Returns a sequence of all the values in m.
-let values m =
-    m |> Map.toSeq |> Seq.map snd
+let values table =
+    table |> Map.toSeq |> Seq.map snd
 
-let merge m2 m =
-    Map.fold (fun acc key value -> Map.add key value acc) m m2
+/// Builds a new map made by adding all the elements of other into table.
+/// Values with the same key will be overwritten.
+let merge other table =
+    Map.fold (fun acc key value -> Map.add key value acc) table other
+
+/// Builds a new map by applying the given function to each element of the table.
+/// The first argument of the function is the index of the element being transformed,
+/// starting from start.
+let mapiFrom start mapping table  = 
+    table
+    |> Map.fold (
+        fun (index, result) k v -> 
+            (index+1, (Map.add k (mapping index k v) result))) (start, Map.empty) 
+    |> snd
+
+/// Builds a new map by applying the given function to each element of the table.
+/// The first argument of the function is the index of the element being transformed,
+/// starting from 0.
+let mapi mapping table =
+    mapiFrom 0 mapping table
