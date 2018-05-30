1a0e6b0a291ccff516e39f1d80db70c926923e59
Luca Di Stefano <luca.distefano@gssi.it>
2018-05-30 15:03:26 +0200

    Rename labs.py to sliver.py

--- linux/sliver.py
+++ linux/sliver.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import begin
 import re
+import platform
+import sys
 from subprocess import check_output, DEVNULL, CalledProcessError
 from enum import Enum
 from os import remove
 import uuid
 from cex import translateCPROVER
+
+SYS = platform.system()
 
 
 class Backends(Enum):
@@ -19,6 +23,8 @@ Options are: {}""".format(", ".join(b.value for b in Backends))
 timeout_descr = """configure time limit (seconds).
 A value of 0 means no timeout (default)"""
 
+values_descr = "specify values for parameterised specification (key=value)"
+
 backends = {
     "cbmc": ["cbmc"],
     "esbmc": [
@@ -26,6 +32,21 @@ backends = {
         "--no-pointer-check", "--no-align-check", "--no-unwinding-assertions",
         "--z3"]
 }
+
+backends_debug = {
+    "cbmc": ["cbmc", "--bounds-check" "--signed-overflow-check"],
+    "esbmc": [
+        "esbmc", "--no-pointer-check", "--no-align-check",
+        "--no-unwinding-assertions", "--z3"]
+}
+
+
+def check_cbmc_version():
+    cbmc_check = ["cbmc", "--version"]
+    CBMC_V, CBMC_SUBV = check_output(cbmc_check).decode().strip().split(".")
+    if not (int(CBMC_V) <= 5 and int(CBMC_SUBV) <= 4):
+        backends["cbmc"].append("--trace")
+        backends_debug["cbmc"].append("--trace")
 
 
 def unwind(backend, num):
@@ -40,8 +61,14 @@ def unwind(backend, num):
     }[backend]
 
 
-cmd = "./LabsTranslate"
-env = {"LD_LIBRARY_PATH": "libunwind"}
+cmd = "core/LabsTranslate"
+
+if "Linux" in SYS:
+    env = {"LD_LIBRARY_PATH": "core/libunwind"}
+    TIMEOUT_CMD = "/usr/bin/timeout"
+else:
+    env = {}
+    TIMEOUT_CMD = "/usr/local/bin/gtimeout"
 
 
 class Components:
@@ -79,7 +106,7 @@ def gather_info(call):
     return info
 
 
-def parse_linux(file, values, bound, fair):
+def parse_linux(file, values, bound, fair, simulate):
     call = [
         cmd,
         "--file", file,
@@ -88,6 +115,8 @@ def parse_linux(file, values, bound, fair):
         call.extend(["--values"] + list(values))
     if fair:
         call.append("--fair")
+    if simulate:
+        call.append("--simulation")
     try:
         out = check_output(call, env=env)
         fname = str(uuid.uuid4()) + ".c"
@@ -96,6 +125,7 @@ def parse_linux(file, values, bound, fair):
             out_file.write(out)
         return out.decode("utf-8"), fname, gather_info(call)
     except CalledProcessError as e:
+        print(e, file=sys.stderr)
         return None, None, None
 
 
@@ -115,39 +145,55 @@ def main(file: "path to LABS file",
          backend: backend_descr = "cbmc",
          steps: "number of system evolutions" = 1,
          fair: "enforce fair interleaving of components" = True,
+         simulate: "run in simulation mode" = False,
+         show: "print C encoding and exit" = False,
+         debug: "enable additional checks in the backend" = False,
          timeout: timeout_descr = 0,
-         *values: "specify the value of placeholders (key=value)"):
-    """ Verification of multi-agent systems in the LABS format.
+         *values: values_descr):
+    """ SLiVER - Symbolyc LAbS VERification.
 """
-    print("Encoding...")
-    c_program, fname, info = parse_linux(file, values, steps, fair)
+    print("Encoding...", file=sys.stderr)
+    c_program, fname, info = parse_linux(file, values, steps, fair, simulate)
     if fname:
-        backend_call = backends[backend]
+        if show:
+            print(c_program)
+            return
+        if backend == "cbmc":
+            check_cbmc_version()
+        backend_call = backends_debug[backend] if debug else backends[backend]
         backend_call.append(fname)
         backend_call.extend(unwind(backend, info["unwind"]))
         if timeout > 0:
-            backend_call = ["/usr/bin/timeout", str(timeout)] + backend_call
+            backend_call = [TIMEOUT_CMD, str(timeout)] + backend_call
         try:
-            print("Verifying with backend {}...".format(backend))
+            sim_or_verify = "Running simulation" if simulate else "Verifying"
+            print(
+                "{} with backend {}...".format(sim_or_verify, backend),
+                file=sys.stderr)
             out = b''
             out = check_output(backend_call, stderr=DEVNULL)
         except KeyboardInterrupt as err:
-            print("Verification stopped (keyboard interrupt)")
+            print("Verification stopped (keyboard interrupt)", file=sys.stderr)
         except CalledProcessError as err:
             if err.returncode == 10:
-                out = err.check_out
+                out = err.output
             elif err.returncode == 6:
-                print("Backend failed with parsing error.")
+                print("Backend failed with parsing error.", file=sys.stderr)
             elif err.returncode == 124:
-                print("Timeout")
+                print(
+                    "Timed out after {} seconds"
+                    .format(timeout), file=sys.stderr)
+            else:
+                print(
+                    "Unexpected error (return code: {})"
+                    .format(err.returncode), file=sys.stderr)
         finally:
             out = out.decode("utf-8")
             remove(fname)
-
             if ("VERIFICATION SUCCESSFUL" in out):
-                print("VERIFICATION SUCCESSFUL")
+                print("No properties violated!", end="", file=sys.stderr)
+                if simulate:
+                    print(" (simulation mode)", file=sys.stderr)
             else:
                 print(translateCPROVER(out, c_program, info))
                 # cex_cbmc(out, i_names, l_names)
-
-# PASS/FAIL/TIMEOUT/UNKNOWN
