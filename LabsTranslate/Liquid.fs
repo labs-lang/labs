bd1c1bb5f65ca4a4b9b0bedc42e67e982459e554
Luca Di Stefano <luca.distefano@gssi.it>
2018-07-16 09:41:55 +0200

    Add DotLiquid helper functions

---
+++ LabsTranslate/Liquid.fs
@@ -0,0 +1,19 @@
+﻿module internal Liquid
+
+open Base
+open DotLiquid
+
+let makehash (l: ('a * 'b) list) =
+        l
+        |> List.map (fun (a,b) ->
+            dict ["name", box a; "value", box b]
+            |> Hash.FromDictionary)
+        |> box
+
+///<summmary>Opens a template file and renders it using the specified
+///local variables.</summary>
+let renderFile path (data:(string * obj) list) = 
+    readFile(path)
+    |> Result.map Template.Parse
+    |> Result.map (fun x -> x.Render (data |> dict |> Hash.FromDictionary))
+    |> Result.map (printfn "%s")
