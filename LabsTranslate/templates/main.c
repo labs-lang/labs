28318eec85d2c5626f6bde222ceb006808976f60
Luca Di Stefano <luca.distefano@gssi.it>
2018-07-16 09:48:05 +0200

    Fix main template

--- LabsTranslate/templates/main.c
+++ LabsTranslate/templates/main.c
@@ -2,16 +2,16 @@ int main(void) {
     init();
     unsigned char choice[BOUND];
     int __LABS_step;
-    {% if fair == 1 -%}unsigned char last;{% endif %}
+    {% if fair -%}unsigned char last;{% endif %}
     for (__LABS_step=0; __LABS_step<BOUND; __LABS_step++) {
         if (all_term()) break;
     
         __VERIFIER_assume(choice[__LABS_step] < MAXCOMPONENTS + 2);
     
         if (choice[__LABS_step] < MAXCOMPONENTS) {
-            {% if fair == 1 -%}__VERIFIER_assume(choice[__LABS_step] == last+1 || (last == MAXCOMPONENTS - 1 && choice[__LABS_step] == 0));{% endif %}
+            {% if fair -%}__VERIFIER_assume(choice[__LABS_step] == last+1 || (last == MAXCOMPONENTS - 1 && choice[__LABS_step] == 0));{% endif %}
 {{schedule}}
-            {% if fair == 1 -%}last = choice[__LABS_step];{% endif %}
+            {% if fair -%}last = choice[__LABS_step];{% endif %}
         }
         else if (choice[__LABS_step] == MAXCOMPONENTS) 
             propagate();
