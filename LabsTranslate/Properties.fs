f605424f28ec52bf4d1076b1538be9331bc804de
Luca Di Stefano <luca.distefano@gssi.it>
2018-10-26 15:46:10 +0200

    Return to first-order encoding of properties
    The previous approach (using assumptions for "forall"
    properties) would not work correctly in simulation mode.

--- LabsTranslate/Properties.fs
+++ LabsTranslate/Properties.fs
@@ -3,22 +3,10 @@ open Types
 open Templates
 open Expressions
 
-/// Cartesian product of a list of lists.
-//http://techneilogy.blogspot.com/2010/07/cartesian-product-of-lists-in-f.html
-let rec cart nll = 
-    let f0 n = function
-    | [] -> [[n]]
-    | nll -> List.map (fun nl->n::nl) nll
-    match nll with
-    | [] -> []
-    | h::t -> List.collect (fun n->f0 n (cart t)) h
-    
 let translateProp sys (p:Property<Var*int>) =
 
     let trId (sub:Map<string, int>) name = 
-        match snd p.quantifiers.[name] with 
-        | All -> name+p.name
-        | Exists -> (sprintf "%i" sub.[name])
+        string sub.[name]
 
     //Given a substitution table, resolves references to quantified
     //component names.
@@ -39,43 +27,30 @@ let translateProp sys (p:Property<Var*int>) =
         filterUndef = fun ((v, _), _) -> v.init = Undef
     }
 
-    let exists, forall = 
-        Map.partition
-            (fun _ (_, q) -> match q with Exists -> true | _ -> false)
-            p.quantifiers
-    if not (exists.IsEmpty || forall.IsEmpty) then 
+    let ex = Map.exists (fun _ (_, q)-> q = Exists) p.quantifiers
+    let fa = Map.exists (fun _ (_, q)-> q = All) p.quantifiers
+
+    if (ex && fa) then 
         p.name
         |> sprintf "Property %s: alternating quantifiers are currently not supported"
         |> failwith
 
-    let subs =
-        exists
-        |> Map.mapValues (fun (cmpType, _) -> sys.spawn.[cmpType])
-        |> Map.toList
-        |> List.map (fun (id, (starts, ends)) ->
-            [starts..ends-1] |> List.map (fun i -> id,i))
-        |> cart
-        |> List.map Map.ofList
-        |> fun x -> if x.IsEmpty then [Map.empty] else x
-        
     let translateSub sub =
         (predExpr sub).BExprTranslator ("Property " + p.name)
 
-    let assumptions =
-        forall
-        |> Map.map (
-            fun id (cmpType, _) ->
-                let varName = id + p.name
-                let cmin, cmax = sys.spawn.[cmpType]
-                sprintf "%s >= %i && %s < %i" varName cmin varName cmax
-                |> assume 
-                |> (sprintf "int %s;\n%s" varName)
-            )
-        |> Map.values
-        |> String.concat ""
-    subs
-    |> List.map (fun s -> translateSub s p.predicate)
-    |> String.concat " || "
+    let rec trProp subs prop =
+        if not prop.quantifiers.IsEmpty then
+            let nextId = Map.pick (fun k _ -> Some k) prop.quantifiers
+            let agent, quantifier = prop.quantifiers.[nextId]
+            let amin, amax = sys.spawn.[agent]
+
+            [amin..amax-1]
+            |> List.map (fun i -> Map.add nextId i subs)
+            |> List.map (fun s -> trProp s {prop with quantifiers=prop.quantifiers.Remove nextId})
+            |> String.concat(match quantifier with All -> " && " | Exists -> " || ")
+        else
+            translateSub subs prop.predicate
+
+    trProp Map.empty p
     |> fun pstring -> inlineassertion pstring p.name
     |> fun x -> sprintf "%s //%s\n" x p.name
-    |> (+) assumptions
