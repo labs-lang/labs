a8de3f5ffc666be51deb9f6104ca19694078a6ab
Luca Di Stefano <luca.distefano@gssi.it>
2019-02-22 16:46:19 +0100

    Improve parse logs with <!> (Debug mode)

--- LabsParser/Common.fs
+++ LabsParser/Common.fs
@@ -51,7 +51,7 @@ let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
     fun stream ->
         eprintfn "%A: Entering %s" stream.Position label
         let reply = p stream
-        eprintfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+        eprintfn "%A: Leaving %s (%A: %O)" stream.Position label reply.Status reply.Result
         reply
     #else
     p
