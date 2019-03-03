51f304a74516976bdbeffbc0ff04c28b938ef3eb
Luca Di Stefano <luca.distefano@gssi.it>
2019-03-03 17:01:21 +0100

    Add Tokens file
    This will contain LAbS tokens than must be accessed from
    multiple places.
    Warning: These are NOT to be used in C code generation!

---
+++ LabsCore/Tokens.fs
@@ -0,0 +1,21 @@
+﻿module Tokens
+
+let tPLUS = "+"
+let tMINUS = "-"
+let tMUL = "*"
+let tDIV = "/"
+let tMOD = "%"
+let tUNDEF = "undef"
+let tABS = "abs"
+let tID = "id"
+
+let tTRUE = "true"
+let tFALSE = "false"
+let tNEG = "!"
+let tCONJ = "and"
+let tDISJ = "or"
+
+let tSEQ = ";"
+let tCHOICE = "++"
+let tPAR = "||"
+let tGUARD = "->"
