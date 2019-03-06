09bc775964025288fbd8db4f6fce65ffd2ab50cd
Luca Di Stefano <luca.distefano@gssi.it>
2019-03-06 14:20:45 +0100

    Remove unused code from Result.cs

--- LabsTranslate/Result.fs
+++ LabsTranslate/Result.fs
@@ -1,1 +1,22 @@
-﻿
+﻿module Result
+
+let foldErrors joinfn acc source = 
+    let fn a = function | Ok _ -> a | Error e -> (joinfn a e)
+    Seq.fold fn acc source
+
+let setReturnCode r =
+    match r with 
+    | Ok _ -> 0
+    | Error _ -> 10
+
+let log msg = function 
+    | Ok a -> 
+        eprintfn "\n%s" msg
+        Ok a
+    | Error e -> Error e
+
+let logErr = function
+    | Ok a -> Ok a
+    | Error s -> 
+        eprintfn "[Error] %s" s
+        Error ""
