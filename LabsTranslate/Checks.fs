fdc5b97de86b3108641830f6f5b3b3176124243e
Luca Di Stefano <luca.distefano@gssi.it>
2019-03-06 11:20:42 +0100

    Fix Checks.fs to work with new types

--- LabsTranslate/Checks.fs
+++ LabsTranslate/Checks.fs
@@ -1,13 +1,15 @@
 ﻿module internal Checks
 open Types
+open LabsCore
 open Link
 open Base
+open FSharpPlus
 
 /// Performs several checks related to components
 let checkComponents sys =
     let undefBehaviors = 
         sys.components
-        |> Map.filter (fun _ def -> not <| def.processes.ContainsKey "Behavior")
+        |> Map.filter (fun _ def -> not <| Map.containsKey "Behavior" def.processes)
     if sys.components.IsEmpty then
         Result.Error "No agents defined"
     else
@@ -24,39 +26,32 @@ let checkComponents sys =
 
 /// Verifies that all process names in the program have been defined.
 let checkNames sys =
-    let usedNames = 
-        let basefn fn b = 
-            match b with 
-            | Name(s, _) -> Set.singleton b
-            | Guard(_, p, _)
-            | Paren(p, _) -> fn p
-            | _ -> Set.empty
-        walk basefn Set.unionMany Set.unionMany Set.unionMany
-    
-    let check (definitions: Map<string, 'a PROC>) =
-        let formatErrorMsg (b: 'a BASE) =
-            sprintf "%A Undefined process: %s" b.Pos (string b)
-        let used = 
-            (Map.values definitions)
-            |> Seq.map usedNames
-            |> Set.unionMany
-        let undefs = 
-            Set.filter
-                (fun n -> not <| Map.containsKey (string n) definitions) used
-        if undefs.IsEmpty then Ok()
-        else 
-            undefs
-            |> Set.map formatErrorMsg
-            |> String.concat "\n"
-            |> Error
-
+    let check definitions =
+        let formatErrorMsg where n =
+            sprintf "%s: Undefined name %s" where n
+        let isDefined n = Map.containsKey n definitions
+        definitions
+        |> Map.mapValues Process.usedNames
+        |> Map.map (fun k used ->
+            let undefs = Set.filter (not << isDefined) used
+            if undefs.IsEmpty then Ok()
+            else 
+                undefs
+                |> Set.map (formatErrorMsg k)
+                |> String.concat "\n"
+                |> Error
+        )
+        |> Map.values
+        |> Result.foldErrors (sprintf "%s\n%s") ""
+        |> fun x -> if x = "" then Ok() else Error x
+        
     let checkAgent (a:ComponentDef<_>) =
         check (Map.merge sys.processes a.processes)
 
     Map.mapValues checkAgent sys.components
     |> Map.values
-    |> Seq.append (Seq.singleton (check sys.processes))
-    |> Seq.reduceBack (<&&>)
+    |> if sys.processes.IsEmpty then id else Seq.append (Seq.singleton (check sys.processes))
+    |> Seq.reduceBack (fun a b -> a >>= fun _ -> b)
 
 let analyzeKeys sys = 
     let comps = Map.values sys.components
@@ -67,8 +62,6 @@ let analyzeKeys sys =
             |> Set.unionMany
             |> List.ofSeq
             |> List.groupBy (fun x -> x.name)
-            //|> Set.unionMany
-            //|> Seq.map (fun k -> k, (seqOfVars |> Seq.choose (Map.tryFind k) |> List.ofSeq))
             |> Map.ofSeq
 
         groups
@@ -125,5 +118,84 @@ let analyzeKeys sys =
     attrKeys
     |> Map.mergeIfDisjoint lstigkeys
     |> Map.mergeIfDisjoint envKeys
-    |> Ok
-    >+> Ok (max maxI 1, max maxL 1, max maxE 1)
+    |> fun x -> Ok (sys, x, max maxI 1, max maxL 1, max maxE 1)
+
+
+/// Binds all references in the system to the corresponding variable.
+let toVarSystem (sys:SystemDef<string>) (mapping:KeyMapping) =
+
+    let addUndefs (cmp:ComponentDef<_>) =
+        let vname_ (v:Var) = v.name
+        let undefs : Set<Var> = 
+            let names = Set.map vname_ cmp.iface
+            Set.filter (not << names.Contains << vname_) sys.ifaceVars.Value
+        {cmp with iface = Set.union cmp.iface undefs}
+
+    let allVars =
+        sys.ifaceVars.Value 
+        |> Set.union sys.lstigVars.Value
+        |> Set.union sys.environment
+        |> Set.map (fun v -> v.name, v)
+        |> Map.ofSeq
+
+    let findString k = 
+        match allVars.TryFind k with
+        | Some v -> v, mapping.[v.name]
+        | None -> failwith (sprintf "Undefined variable: %s" k)
+
+    let toVarRef f r o =
+        {var=f r.var; offset=o}
+    let toVarExpr f e = 
+        Expr.map (Leaf) (fun r o -> Ref(toVarRef f r o)) e
+    
+    let toVarBase f b =    
+        match b.stmt with 
+        | Act a ->
+            let newupdates =
+                a.updates
+                |> List.map (fun (r, e) -> 
+                    toVarRef f r (Option.map (toVarExpr f) r.offset), toVarExpr f e)
+            {stmt=Act {actionType=a.actionType; updates=newupdates}; pos=b.pos}
+        | Nil -> {stmt=Nil; pos=b.pos}
+        | Skip -> {stmt=Skip; pos=b.pos}
+        | Name s -> {stmt=Name s; pos=b.pos}
+
+    let toVarBExpr f b =
+        BExpr.map (BLeaf) (toVarExpr f) b
+
+    let toVarProcess proc = 
+        Process.map ((toVarBase findString) >> BaseProcess) (toVarBExpr findString) proc
+        
+    let toVarComponent (c:ComponentDef<string>) =
+        {
+            name = c.name
+            iface = c.iface
+            lstig = c.lstig
+            processes = c.processes |> Map.mapValues toVarProcess
+        }
+
+    let toVarStigmergy lstig =
+        {
+            name = lstig.name
+            link = lstig.link |> (toVarBExpr (fun (x,y) -> findString x, y))
+            vars = lstig.vars
+        }
+
+    let toVarProperty (pr:Property<string>) = 
+        {
+            name=pr.name
+            predicate=pr.predicate |> (toVarBExpr (fun (x,y) -> findString x, y))
+            modality=pr.modality
+            quantifiers=pr.quantifiers
+        }
+    
+    {
+        components =
+            sys.components
+            |> Map.mapValues (addUndefs >> toVarComponent)
+        stigmergies = sys.stigmergies |> Map.mapValues toVarStigmergy
+        environment = sys.environment 
+        processes = sys.processes |> Map.mapValues toVarProcess
+        spawn = sys.spawn
+        properties = sys.properties |> Map.mapValues toVarProperty
+    }
