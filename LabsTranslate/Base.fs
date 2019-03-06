a7b0542464ea7d08c26bcab0cf2dfe6d6387b12f
Luca Di Stefano <luca.distefano@gssi.it>
2019-03-06 11:17:17 +0100

    Use FSharpPlus operators

--- LabsTranslate/Base.fs
+++ LabsTranslate/Base.fs
@@ -1,52 +1,14 @@
 ﻿module internal Base 
 open FParsec
-open System.IO
+open FSharpPlus.Operators
+open LabsCore
 open Parser
 
 type KeyMapping = Map<string, int>
 
-/// Bind operator for Result.
-#if DEBUG
-let inline (>>=) r f = try Result.bind f r with ex -> Result.Error (ex.Message + ex.StackTrace)
-#else
-let inline (>>=) r f = try Result.bind f r with ex -> Result.Error (ex.Message)
-#endif
-
-// Returns r2 if r1 is Ok, else returns r1
-let inline (<&&>) r1 r2 = r1 >>= (fun _ -> r2)
-
-let setReturnCode r =
-    match r with 
-    | Result.Ok _ -> 0
-    | Result.Error _ -> 10
-
-/// Puts the results of r2 and r1 in a new Result
-let (>+>) r2 r1 =
-    match r2, r1 with
-    | Result.Ok a, Result.Ok b -> Result.Ok (a, b)
-    | Result.Error err, _
-    | _, Result.Error err -> Result.Error err
-    
-let log msg r = 
-    match r with
-    | Result.Ok _ -> 
-        eprintfn "\n%s" msg
-        r
-    | Result.Error _ -> r
-
-let logErr = function
-    | Result.Ok a -> Result.Ok a
-    | Result.Error s -> 
-        eprintfn "[Error] %s" s
-        Result.Error ""
-
-let readFile filepath =
-    Result.Ok filepath
-    >>= (Result.Ok << File.ReadAllText)
-
 let withcommas x = (String.concat ", " x)
 
-let parse (text, (placeholders:Map<string, string>)) =
+let parse text (placeholders:Map<string, string>) =
     let checkPlaceholders s =
         (Map.keys placeholders)
         |> Set.difference s
@@ -57,7 +19,8 @@ let parse (text, (placeholders:Map<string, string>)) =
 
     let wrapParserResult p text = 
         try
-            match CharParsers.run p text with
+            let x = CharParsers.run p text
+            match x with
             | Success(a, _, _) -> Result.Ok a
             | Failure(errorMsg, _, _) -> 
                 Result.Error (sprintf "Parsing failed:\n %s" errorMsg)
@@ -68,32 +31,32 @@ let parse (text, (placeholders:Map<string, string>)) =
         CharParsers.run stripComments text
         |> function | Success(a, _, _) -> a | Failure(msg,_,_) -> failwith msg
 
-    let defPlaceholders = 
+    let defPlaceholders =    
         stripped
         |> (wrapParserResult pre)
-        |> Result.map (Set.filter ((<>) ""))
+        |>> (Set.filter ((<>) ""))
         >>= checkPlaceholders
-        >+> (wrapParserResult allPlaceholders stripped |> Result.map Set.ofList |> Result.map (Set.filter ((<>) "")))
-        >>= fun (def, all) -> 
-            let diff = (Set.difference all def)
-            if diff.IsEmpty
-            then Result.Ok def
-            else
+        >>= fun def ->
+            // Check for undefined external variables
+            wrapParserResult allPlaceholders stripped
+            |>> Set.ofList |>> (Set.filter ((<>) ""))
+            |>> fun all -> (Set.difference all def)
+            >>= (fun diff ->
+                if Set.isEmpty diff then Result.Ok def else 
                 diff
                 |> Set.map ((+) "_")
                 |> withcommas
                 |> sprintf "External variables %s have not been defined in the 'extern' section." 
-                |> Result.Error
+                |> Result.Error)
 
     defPlaceholders
-    |> Result.map ((Set.fold (fun (txt:string) ph -> txt.Replace("_"+ph, placeholders.[ph])) stripped))
-    >>= (wrapParserResult parse)
+    |>> ((Set.fold (fun (txt:string) ph -> txt.Replace("_"+ph, placeholders.[ph])) stripped))
+    >>= (wrapParserResult Parser.parse)
 
 let enumerate s = 
     s
     |> Seq.mapi (fun i x -> x, i)
     |> Map.ofSeq
-
 
 let makeCounter (start: int) =
     let x = ref start
@@ -102,3 +65,19 @@ let makeCounter (start: int) =
         !x
     incr
 
+let unfold (procs: Map<_, _>) name =
+    // Keep track of the unfolded process
+    let chpos name pos (b:Base<_,FParsec.Position>) =
+        let p = b.pos in let newPos = Position(sprintf "%s@%O" name pos, p.Index, p.Line, p.Column)
+        BaseProcess {b with pos = newPos}
+        
+    let rec unfold_ visited name = 
+        let base_ b = 
+            match b.stmt with
+            | Name n when n=name || n="Behavior" -> BaseProcess b
+            | Name n when (not (Set.contains b visited)) -> 
+                unfold_ (visited.Add b) n 
+                |> Process.map (chpos n b.pos) id
+            | _ -> BaseProcess b
+        Process.map base_ id procs.[name]
+    unfold_ Set.empty name
