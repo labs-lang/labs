75f7ef031de683ac0db3d9b21abd42184ac19bfd
Luca Di Stefano <luca.distefano@gssi.it>
2019-05-14 16:12:12 +0200

    Add Labs-to-C translation of properties
    Some of this code should eventually be moved to the Frontend

--- LabsToC/Expressions.fs
+++ LabsToC/Expressions.fs
@@ -1,4 +1,5 @@
 ﻿module internal LabsToC.Expressions
+open Frontend
 open LabsCore
 open Types
 
@@ -10,6 +11,7 @@ let translateLocation = function
 let translateInitLocation a b c = (sprintf "_%s") (translateLocation a b c)
 
 let refTypeCheck v (offset:'a option) =
+    // TODO move to frontend
     let test, msg = 
         match v.vartype with
         | Scalar -> offset.IsSome, (sprintf "Scalar %s treated as Array")
@@ -19,7 +21,7 @@ let refTypeCheck v (offset:'a option) =
 /// Returns the set of all stigmergy variables accessed by the expression.
 let getLstigVars expr =
     Expr.getVars expr
-    |> Set.filter (fun (v, _) -> match v.location with L _ -> true | _ -> false)
+    |> Set.filter (fun (v, _) -> isLstigVar v)
 
 let private translate trRef trId =
     let leaf_ = function
@@ -56,7 +58,7 @@ let checkUndef filter trref expr =
     |> Seq.map (sprintf "(%s != undef_value)")
     |> fun s -> if Seq.isEmpty s then "" else String.concat " & " s
 
-let rec private translateBExpr filter trref trExpr bexpr checkundef =
+let rec private translateBExpr filter trref trExpr checkundef bexpr =
     let bleaf_ b = if b then "1" else "0"
     let neg_ = sprintf "(!(%s))"
     let compound_ = function
@@ -69,7 +71,7 @@ let rec private translateBExpr filter trref trExpr bexpr checkundef =
         |> (if undef2 <> "" && checkundef then sprintf "(%s) & (%s)" undef2 else id)
     BExpr.cata bleaf_ neg_ compare_ compound_ bexpr
     
-type TranslateFactory<'a, 'b> when 'a:comparison and 'b:comparison = {
+type Translator<'a, 'b> when 'a:comparison and 'b:comparison = {
     refTranslator: 'a -> (string option) -> string
     idTranslator: 'b -> string
     filterUndef: 'a -> bool
@@ -107,3 +109,55 @@ let linkExpr =
         idTranslator= trLinkId
         filterUndef= fun ((v, (_:int)), _) -> v.init = Undef
     }
+    
+let translateProp (table:SymbolTable) (p:Node<Property<_>>) =
+    //TODO move checks to frontend 
+    let trId (sub:Map<_,_>) name =
+        match sub.TryFind name with
+        | Some e -> string e
+        | None -> failwithf "Undefined agent %s" name 
+        
+    //Given a substitution table, resolves references to quantified
+    //component names.
+    let tr (sub:Map<string, int>) ((v, i), c) offset =
+        match c with
+        | None -> 
+            if v.location <> E then
+                v.name
+                |> failwithf "Property %s: %s is not an environment variable" p.name
+            trref translateLocation "" (v, i) offset
+        | Some c ->
+            (trref translateLocation (trId sub c) (v, i) offset)
+
+    let predExpr sub = {
+        refTranslator = tr sub
+        idTranslator = trId sub
+        filterUndef = fun ((v, _), _) -> v.init = Undef
+    }
+
+    let ex = Map.exists (fun _ (_, q)-> q = Exists) p.def.quantifiers
+    let fa = Map.exists (fun _ (_, q)-> q = All) p.def.quantifiers
+
+    if (ex && fa) then 
+        p.name
+        |> failwithf "Property %s: alternating quantifiers are currently not supported"
+
+    let translateSub sub =
+        (predExpr sub).BExprTranslator false
+
+    let rec trProp subs prop =
+        if not prop.quantifiers.IsEmpty then
+            let nextId = Map.pick (fun k _ -> Some k) prop.quantifiers
+            let agent, quantifier = prop.quantifiers.[nextId]
+            let amin, amax = table.spawn.[agent]
+
+            [amin..amax-1]
+            |> List.map (fun i -> Map.add nextId i subs)
+            |> List.map (fun s -> trProp s {prop with quantifiers=prop.quantifiers.Remove nextId})
+            |> String.concat(
+                match quantifier with 
+                | All -> " & "
+                | Exists -> " | ")
+        else
+            translateSub subs prop.predicate
+    trProp Map.empty p.def
