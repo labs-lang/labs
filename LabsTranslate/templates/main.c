7e31cc878700e1b94b0722d84297671d3f5d5efc
Luca Di Stefano <luca.distefano@gssi.it>
2019-09-13 15:10:30 +0200

    Generate unbounded C code when BOUND=0

--- LabsTranslate/templates/main.c
+++ LabsTranslate/templates/main.c
@@ -18,29 +18,39 @@ int main(void) {
     TYPEOFAGENTID firstAgent{% if firstagent == 0 and fair %} = 0{% endif %};
 
     #if DISABLELSTIG == 0
+        #if BOUND > 0
     Bool sys_or_not[BOUND];
+        #endif
     #endif
 
+    #if BOUND > 0
     unsigned __LABS_step;
     for (__LABS_step=0; __LABS_step<BOUND; __LABS_step++) {
+    #else
+    while(1) {        
+    #endif
         // if (terminalState()) break;
         
-        // _Bool sys_or_not;
         #if DISABLELSTIG == 0
+            #if BOUND > 0
         if (sys_or_not[__LABS_step]) {
+            #else
+        if ((Bool) __VERIFIER_nondet()) {
+            #endif
         #endif
             LABSassume(firstAgent < MAXCOMPONENTS);
 
+            #if BOUND > 0
+            switch (switchnondet[__LABS_step]) {
+            #else
+                switch (__VERIFIER_nondet()) {
+            #endif
+
             {%- for item in schedule -%}
-            {% unless forloop.first %}else {% endunless %}if LABScheck({%- for pc in item.entry -%}
-pc[firstAgent][{{pc.name}}] == {{pc.value}}{% unless forloop.last %} & {% endunless %}{%- endfor -%}
-{%- if item.siblings.size != 0 -%} &
-{%- for pc in item.siblings -%}
-pc[firstAgent][{{pc}}] {%-if item.name contains 'last'-%}=={%- else -%}!={%- endif -%} 0{% unless forloop.last %} & {% endunless %}
-{%- endfor -%}
-{%- endif -%}
-, {{ item.guards | join: " & " }}) {{ item.name }}(firstAgent);
-{%- endfor -%}
+                case {{ forloop.index0 }}: {{ item.name }}(firstAgent); break;
+            {%- endfor -%}
+              default: LABSassume(0);
+            }
             
             {%- if fair -%}
             if (firstAgent == MAXCOMPONENTS - 1) {
