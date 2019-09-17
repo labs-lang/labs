fbabec176826b0229d92960a5394c05423eac5fb
Luca Di Stefano <luca.distefano@gssi.it>
2019-09-17 18:45:51 +0200

    Remove sliver dir

--- sliver/__init__.py
+++
@@ -1,3 +0,0 @@
-from __about__ import (__title__, __version__, __date__)
-
-__all__ = [__title__, __version__, __date__]
--- sliver/cseq/core/config.py
+++
@@ -1,5 +0,0 @@
-# directory where to save debug files, cannot be empty, do not use trailing /
-debugpath = 'log'
-
-# default analysis
-defaultchain = 'lazy'
