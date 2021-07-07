labs-lang/labs

commit 0f15733d27ae90f0705b95202850a7e4ac448fb3

Add error message when using nondet in LNT

LabsTranslate/TranslationKit.fs
@@ -251,7 +251,7 @@ module internal Lnt =
         let unaryFn = function
             | UnaryMinus -> sprintf "-(%s)"
             | Abs -> sprintf "abs(%s)"
-        let nondetFn = sprintf "nondetInRange(%s, %s)"
+        let nondetFn = failwithf "nondet expressions are currently not supported in LNT"
         Expr.cata leafFn arithmFn unaryFn nondetFn trRef
 
     let rec private trBExprLnt filter trExpr bexpr =

commit b4569277922f3a311a556b4012bf05a801070f88

Fix a bug introduced in 0f15733

LabsTranslate/TranslationKit.fs
@@ -251,7 +251,7 @@ module internal Lnt =
         let unaryFn = function
             | UnaryMinus -> sprintf "-(%s)"
             | Abs -> sprintf "abs(%s)"
-        let nondetFn = failwithf "nondet expressions are currently not supported in LNT"
+        let nondetFn = fun _ _ -> failwithf "nondet expressions are currently not supported in LNT"
         Expr.cata leafFn arithmFn unaryFn nondetFn trRef
 
     let rec private trBExprLnt filter trExpr bexpr =