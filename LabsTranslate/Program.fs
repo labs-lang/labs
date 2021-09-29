532b72aaf7469d98d7026aec9e694d0226efc78c
Luca Di Stefano <luca.distefano@gssi.it>
2021-09-29 15:41:34 +0200

    Pretty-print "failwithf" messages
    (i.e., do not show full stack trace)

--- LabsTranslate/Program.fs
+++ LabsTranslate/Program.fs
@@ -27,25 +27,29 @@ let main argv =
         cli.Contains Simulation,
         cli.Contains Sync,
         cli.Contains No_Properties)
-    
-    zero argv
-    <~> (parseCLI >> zero)
-    <~> fun cli ->
-        let prop = cli.TryGetResult Property
-        let input = File.ReadAllText (cli.GetResult File)
-        let externs = getExterns cli |> Map.mapValues int
-        (wrapParserResult Parser.parse input <~> Frontend.run externs) <~> fun x -> zero (cli, x)
-    <?> (fun (cli, x) ->
-        if cli.Contains Info then zero (x.Dump(prop))
-        else
-            let bound = cli.GetResult (Bound, defaultValue=1)
-            let enc = cli.GetResult (Enc, defaultValue=C)
-            encode enc bound (flags cli) prop x)
-    |> function
-       | Result.Ok (_, warns) ->
-            warns |> List.map(pprintWarn >> eprintfn "%s") |> ignore
-            0
-       | Result.Error (warns, errs) ->
-           warns |> List.map(pprintWarn >> eprintfn "%s") |> ignore
-           errs |> List.map(pprintErr >> eprintfn "%s") |> ignore
-           1 // TODO more expressive error codes 
+    try
+        zero argv
+        <~> (parseCLI >> zero)
+        <~> fun cli ->
+            let prop = cli.TryGetResult Property
+            let input = File.ReadAllText (cli.GetResult File)
+            let externs = getExterns cli |> Map.mapValues int
+            (wrapParserResult Parser.parse input <~> Frontend.run externs) <~> fun x -> zero (cli, x)
+        <?> (fun (cli, x) ->
+            if cli.Contains Info then zero (x.Dump(prop))
+            else
+                let bound = cli.GetResult (Bound, defaultValue=1)
+                let enc = cli.GetResult (Enc, defaultValue=C)
+                encode enc bound (flags cli) prop x)
+        |> function
+           | Result.Ok (_, warns) ->
+                warns |> List.map(pprintWarn >> eprintfn "%s") |> ignore
+                0
+           | Result.Error (warns, errs) ->
+               warns |> List.map(pprintWarn >> eprintfn "%s") |> ignore
+               errs |> List.map(pprintErr >> eprintfn "%s") |> ignore
+               1 // TODO more expressive error codes 
+    with
+    | ex ->
+        eprintfn $"{ex.Message}"
+        1
