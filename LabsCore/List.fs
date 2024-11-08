7184c410787af3da3dc6d4a9bb0254481f856319
Luca Di Stefano <lucadistefano90@gmail.com>
2024-11-08 19:18:12 +0100

    Add List.cartesian

--- LabsCore/List.fs
+++ LabsCore/List.fs
@@ -9,3 +9,21 @@ let duplicatesBy fn lst =
 
 /// Returns a list of elements that appear more than once in lst.
 let duplicates lst = duplicatesBy id lst
+
+///Cartesian product of a variable number of lists.
+/// Input is a list of lists of which the cartesian product is to be constructed;
+/// output is a list that contains the elements of the product set, as lists.
+///https://www.fssnip.net/2A/title/Cartesian-product-of-n-lists
+/// // Takes an input like [[1;2;5];[3;4];[6;7]] and returns
+// [[5; 3; 7]; [2; 3; 7]; [1; 3; 7]; [5; 4; 7]; [2; 4; 7];
+// [1; 4; 7]; [5; 3; 6]; [2; 3; 6]; [1; 3; 6]; [5; 4; 6];
+// [2; 4; 6]; [1; 4; 6]]
+let rec cartesian lstlst =
+    match lstlst with
+    | h::[] ->
+        List.fold (fun acc elem -> [elem]::acc) [] h
+    | h::t ->
+        List.fold (fun cacc celem ->
+            (List.fold (fun acc elem -> (elem::celem)::acc) [] h) @ cacc
+            ) [] (cartesian t)
+    | _ -> []
